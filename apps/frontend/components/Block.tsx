45a7045da90bd18dfac0e13edc46853d2b755093
Josef Malý <josefmalyy@gmail.com>
2022-12-10 17:12:55 +0100

    multiple additions

---
+++ apps/frontend/components/Block.tsx
@@ -0,0 +1,26 @@
+import { BlockStyled } from "../styled/Block.styled";
+
+type BlockPropsTypes = {
+  title: string;
+  previewImage: string;
+  date: string;
+  file: string;
+};
+
+const Block = (props) => {
+  console.log(props);
+  return (
+    <BlockStyled>
+      <div className="imageContainer">
+        <img className="blockImage" src={props.previewImage} alt="" />
+      </div>
+      <h2 className="blockTitle">{props.title}</h2>
+      <h2 className="blockDate">{props.date}</h2>
+      <a className="blockButton" href={props.file} download={true}>
+        Stáhnout
+      </a>
+    </BlockStyled>
+  );
+};
+
+export default Block;
---
+++ apps/frontend/styled/Presentations.styled.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const StyledPresentations = styled.div`
+  height: 100vh;
+  width: calc(100vw - 8em);
+  margin-left: 8em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .view {
+    width: 95%;
+    height: 90%;
+  }
+`;
